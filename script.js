a40fdee0d4519dd68eede5239d79eedff5d635ac
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-08-11 13:52:56 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function initialize(){
 	let city = document.querySelector("#city").value;
-
+	let degree = "&#176;"
 	let wheather = new XMLHttpRequest();
 	let apiKey = "41d496f5b3f1a854e6e79910b71ba93f";
 	wheather.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city +" &units=imperial"+ "&appid=" + apiKey, false)
@@ -10,10 +10,11 @@ function initialize(){
 	console.log(r)
 	if (city !== "") {
 	let temp = r.main.temp;	
-	let tempInCelcius ="Current Temperature in " + r.name +" is " + (Math.round(Math.round(temp - 32)/1.8)) +" " + "<br />";
-	let display = "Current weather in " + r.name +" "+ r.weather[0].description	 + "<br />";
-	let wind ="Current wind " + r.wind.speed + "<br />";
-		document.getElementById("temp").innerHTML = tempInCelcius;
+	let tempInCelcius = (Math.round(Math.round(temp - 32)/1.8)) ;
+	let display =  r.weather[0].description;
+	let wind = "Wind "+ r.wind.speed;
+		document.querySelector(".city-name").innerHTML = city + "<sup>"+"<i class='fas fa-cloud'>"+"</i>"+"</sup>";
+		document.getElementById("temp").innerHTML = tempInCelcius + degree;
 		document.getElementById("wheather").innerHTML = display;
 		document.getElementById("wind").innerHTML = wind;
 
@@ -21,3 +22,15 @@ function initialize(){
 		alert("Please Enter City Name")
 	}
 }
+
+let bgImgs = ["img1.gif", "img2.gif", "img3.gif", "img4.gif", "img5.gif"];
+
+function themeChanger(){
+
+	setInterval(function(){
+	let bgRound = parseInt(Math.random()*bgImgs.length);
+	document.querySelector(".main").style["background-image"] = `url(images/${bgImgs[bgRound]})`;
+},3000);
+}
+
+themeChanger();
